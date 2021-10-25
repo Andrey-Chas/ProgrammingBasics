6a075f33fb0dc8f49456fb1cfeac910c95d6164d
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-10-25 14:30:28 +0300

    Add files via upload

---
+++ Homework/Homework1/Homework1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Homework1
+    {
+        static void Main(string[] args)
+        {
+            Greeting();
+        }
+        private static void Greeting()
+        {
+            try
+            {
+                Console.Write("Please type a number between 1 and 7: ");
+                int day = int.Parse(Console.ReadLine());
+                Console.Write("Please type time between 0 and 23: ");
+                int time = int.Parse(Console.ReadLine());
+
+                switch (day)
+                {
+                    case 1:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Monday today. Weekend is coming in 5 days");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Monday today. Weekend is coming in 5 days");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Monday today. Weekend is coming in 5 days");
+                        }
+                        break;
+
+                    case 2:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Tuesday today. Weekend is coming in 4 days");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Tuesday today. Weekend is coming in 4 days");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Tuesday today. Weekend is coming in 4 days");
+                        }
+                        break;
+
+                    case 3:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Wednesday today. Weekend is coming in 3 days");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Wednesday today. Weekend is coming in 3 days");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Wednesday today. Weekend is coming in 3 days");
+                        }
+                        break;
+
+                    case 4:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Thursday today. Weekend is coming in 2 days");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Thursday today. Weekend is coming in 2 days");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Thursday today. Weekend is coming in 2 days");
+                        }
+                        break;
+
+                    case 5:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Friday today. Weekend is coming in 1 days");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Friday today. Weekend is coming in 1 days");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Friday today. Weekend is coming in 1 days");
+                        }
+                        break;
+
+                    case 6:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Saturday today. Weekend is here!");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Saturday today. Weekend is here!");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Saturday today. Weekend is here!");
+                        }
+                        break;
+
+                    case 7:
+                        if (time >= 6 && time < 12)
+                        {
+                            Console.WriteLine("Good morning, it is a lovely Sunday today. Weekend is here!");
+                        }
+                        else if (time >= 12 && time < 18)
+                        {
+                            Console.WriteLine("Good day, it is a lovely Sunday today. Weekend is here!");
+                        }
+                        else if (time >= 18 || time >= 0)
+                        {
+                            Console.WriteLine("Good evening, it is a lovely Sunday today. Weekend is here!");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("This day doesn't exist");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework/Homework2/Homework2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Validation
+{
+    class Homework2
+    {
+        static void Main(string[] args)
+        {
+            Validation();
+
+            Console.ReadKey();
+        }
+
+        private static void Validation()
+        {
+            Console.Write("Please type your first name (required, max length = 100): ");
+            string firstName = Console.ReadLine();
+
+            int num;
+            bool firstNameIsNum = int.TryParse(firstName, out num);
+
+            if (firstName.Length >= 100)
+            {
+                Console.WriteLine("The first name is too large");
+                Exit();
+            }
+
+            else if (firstName == "")
+            {
+                Console.WriteLine("This field is required");
+                Exit();
+            }
+
+            else if (firstNameIsNum)
+            {
+                Console.WriteLine("You typed a number");
+                Exit();
+            }
+
+            else
+            {
+                Console.Write("Please type your last name (required, max length = 100): ");
+                string lastName = Console.ReadLine();
+
+                bool lastNameIsNum = int.TryParse(lastName, out num);
+
+                if (lastName.Length >= 100)
+                {
+                    Console.WriteLine("The last name is too large");
+                    Exit();
+                }
+
+                else if (lastName == "")
+                {
+                    Console.WriteLine("This field is required");
+                    Exit();
+                }
+
+                else if (lastNameIsNum)
+                {
+                    Console.WriteLine("You typed a number");
+                    Exit();
+                }
+
+                else
+                {
+                    try
+                    {
+                        Console.Write("Please type your date of birth\n(in the form of DD.MM.YYYY, max years = 100): ");
+                        DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+
+                        string dateOfBirthYears = dateOfBirth.ToString("yyyy");
+                        int dateOfBirthYearsConverted = Convert.ToInt32(dateOfBirthYears);
+
+                        DateTime years = DateTime.Now;
+
+                        var differenceInYears = years.AddYears(-dateOfBirthYearsConverted);
+
+                        if (differenceInYears.Year >= 100)
+                        {
+                            Console.WriteLine("Years exceed the limit");
+                            Exit();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Please type your student number\nin the form YYYYFFSSNNNN(length = 12): ");
+                            string studentNumber = Console.ReadLine();
+
+                            if (studentNumber.Length == 12)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine($"Your information:\n\tFirts name: {firstName}\n\tLast name: {lastName}\n\tDate of birth: {dateOfBirth:dd.MM.yyyy}\n\tStudent number: {studentNumber}");
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Student number length must be 12 characters");
+                                Exit();
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Date of birth was typed in the wrong form");
+                        Exit();
+                    }
+                }
+            }
+        }
+
+        private static void Exit()
+        {
+            try
+            {
+                string a = "a";
+                int num = int.Parse(a);
+            }
+            catch
+            {
+                Console.WriteLine("Please reload the program");
+            }
+        }
+    }
+}
