b4ef867fdfef5cf6e3697ed612012bc3684eba66
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-11-08 13:43:22 +0200

    Add files via upload

---
+++ Homework/Homework3/Homework3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Homework3
+    {
+        static void Main(string[] args)
+        {
+            Romb();
+        }
+
+        private static void Romb()
+        {
+            int n = 5;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int space = 1; space <= n - row; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int column = 1; column <= row; column++)
+                {
+                    Console.Write($"{column}");
+                }
+
+                for (int column1 = row - 1; column1 > 0; column1--)
+                {
+                    Console.Write($"{column1}");
+                }
+
+                Console.WriteLine();
+            }
+
+            n = 10;
+
+            for (int row1 = 6; row1 <= n; row1++)
+            {
+                for (int space = 6; space <= row1; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int column = 1; column <= n - row1; column++)
+                {
+                    Console.Write($"{column}");
+                }
+
+                for (int column1 = 9 - row1; column1 > 0; column1--)
+                {
+                    Console.Write($"{column1}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework/Homework4/Homework4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Homework4
+    {
+        static void Main(string[] args)
+        {
+            Coding();
+        }
+        private static void Coding()
+        {
+            string[] array1 = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
+
+            string[] array2 = { "d", "a", "z", "h", "f", "e", "c", "j", "m", "o", "l", "q", "s", "u", "v", "x", "g", "w", "y", "b", "r", "k", "p", "i", "t", "n", " " };
+
+            Console.Write("Please type a text: ");
+            string text = Console.ReadLine();
+
+            foreach (char letter in text.ToLower())
+            {
+                for (int i = 0; i <= array1.Length; i++)
+                {
+                    if (Convert.ToString(letter) == array1[i])
+                    {
+                        Console.Write(array2[i]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
