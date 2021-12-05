5e3f5e7590ec713ede443512bfa0c90ce23a51f2
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-12-05 15:55:37 +0200

    Add files via upload

---
+++ Homework6/Homework6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework6
+{
+    class Homework6
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+                Name = "Jhon",
+                Surname = "Jonson"
+            };
+
+            student.SetAge(23);
+            student.ShowAge();
+            student.GoToSchool();
+
+            Teacher teacher = new Teacher
+            {
+                Name = "Jack",
+                Surname = "Brown"
+            };
+
+            teacher.Explain();
+
+        }
+    }
+}
