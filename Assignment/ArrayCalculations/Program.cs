a229ddcb5856382dff706308bde8f40bc29bf60a
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2021-12-10 15:46:50 +0200

    Add files via upload

---
+++ Assignment/ArrayCalculations/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ArrayCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates minimum, maximum, sum and avarage\nof the numbers in an array");
+            int[] numbers = { 28, 58, 2, 32, 34, 7, 128, 79, 53, 9, 8, 700, 23, 420, 85, 5 };
+
+            // Converting to string in order to display initial array
+
+            string numbersText = string.Join(",", numbers);
+
+            Console.WriteLine($"Here are numbers: {numbersText}");
+
+            int minInArray = MinInArray(numbers);
+            int maxInArray = MaxInArray(numbers);
+            int sumInArray = SumInArray(numbers);
+            double averageInArray = Math.Round(AverageInArray(numbers), 2);
+            Console.WriteLine($"Minimum number in the array is: {minInArray}");
+            Console.WriteLine($"Maximum number in the array is: {maxInArray}");
+            Console.WriteLine($"Sum of the numbers in the array is: {sumInArray}");
+            Console.WriteLine($"Average of the numbers in the array is: {averageInArray}");
+
+            Console.ReadKey();
+
+        }
+        static public int MinInArray(int[] array)
+        {
+            Array.Sort(array);
+            int minNum = array[0];
+
+            return minNum;
+        }
+
+        static public int MaxInArray(int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+            int maxNum = array[0];
+
+            return maxNum;
+        }
+
+        static public int SumInArray(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+
+            return sum;
+        }
+
+        static public double AverageInArray(int[] array)
+        {
+            double average = 0;
+            double sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+
+            average = sum / array.Length;
+
+            return average;
+        }
+    }
+}
---
+++ Assignment/RemoveDuplicatesFromTheArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicatesFromTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program deletes duplictes from an array");
+            string[] array = { "Hello", "Something", "Car", "Car", "Amazing", "Cat", "Dog", "Cat" };
+
+            string arrayText = string.Join(",", array);
+            Console.WriteLine($"Initial array: {arrayText}");
+            string[] arrayWithoutDuplicates = RemoveDuplicates(array);
+            Console.WriteLine($"Array without duplicates: {string.Join(",", arrayWithoutDuplicates)}");
+
+            Console.ReadKey();
+        }
+
+        static public string[] RemoveDuplicates(string[] array)
+        {
+            List<string> arrayList = new List<string>(array);
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                for (int j = i + 1; j < arrayList.Count; j++)
+                {
+                    if (arrayList[i] == arrayList[j])
+                    {
+                        arrayList.Remove(arrayList[j]);
+                    }
+                }
+            }
+
+            string[] convertToArray = arrayList.ToArray();
+
+            return convertToArray;
+        }
+    }
+}
