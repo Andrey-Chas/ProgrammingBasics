e29d9dc98656e5491a4a9b7e474eab8a21e74339
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2022-01-04 17:01:53 +0200

    Add files via upload

---
+++ Assignment/EmailVerification/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmailVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program checks if email is valid");
+            Console.Write("Please type an email: ");
+            string email = Console.ReadLine();
+
+            string isEmailValid = VerifyEmail(email);
+
+            while (isEmailValid == "")
+            {
+                Console.Write("The email was not entered. Please type an email: ");
+                email = Console.ReadLine();
+                isEmailValid = VerifyEmail(email);
+
+                while (isEmailValid == "Your email is not valid")
+                {
+                    Console.Write("The email is not valid. Please type a valid email: ");
+                    email = Console.ReadLine();
+                    isEmailValid = VerifyEmail(email);
+                }
+            }
+
+            while (isEmailValid == "Your email is not valid")
+            {
+                Console.Write("The email is not valid. Please type a valid email: ");
+                email = Console.ReadLine();
+                isEmailValid = VerifyEmail(email);
+
+                while (isEmailValid == "")
+                {
+                    Console.Write("The email was not entered. Please type an email: ");
+                    email = Console.ReadLine();
+                    isEmailValid = VerifyEmail(email);
+                }
+            }
+
+            Console.WriteLine($"{isEmailValid}");
+            Console.ReadKey();
+        }
+
+        static public string VerifyEmail(string email)
+        {
+            if (email == "")
+            {
+                string value = "";
+
+                return value;
+            }
+
+            else if (new EmailAddressAttribute().IsValid(email))
+            {
+                string isValid = "Your email is valid";
+                return isValid;
+            }
+
+            else
+            {
+                string isNotValid = "Your email is not valid";
+                return isNotValid;
+            }
+        }
+    }
+}
---
+++ Assignment/WordSentenceToUpper/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordSentenceToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program capitalizes a word or a sentence");
+            Console.WriteLine("Please choose: ");
+            Console.WriteLine("\tFor capitalizing a word type 'word'\n\tFor capitalizing a sentence type 'sentence'");
+
+            string userIn = Console.ReadLine();
+            while (userIn.ToLower() != "word" && (userIn.ToLower() != "sentence"))
+            {
+                Console.WriteLine("This option doesn't exist. Please type an existing one.");
+                userIn = Console.ReadLine();
+            }
+
+            if (userIn.ToLower() == "word")
+            {
+                Console.Write("Please type a word: ");
+                userIn = Console.ReadLine();
+                string word = WordToUpper(userIn);
+
+                Console.WriteLine($"{word}");
+                Console.ReadKey();
+            }
+
+            if (userIn.ToLower() == "sentence")
+            {
+                Console.Write("Please type a sentence: ");
+                userIn = Console.ReadLine();
+                string sentence = SentenceToUpper(userIn);
+                string sentenceThroughWord = SentenceThroughWord(userIn);
+
+                Console.WriteLine($"{sentence} through the own function");
+                Console.WriteLine($"{sentenceThroughWord} through the word function");
+                Console.ReadKey();
+            }
+        }
+
+        static public string WordToUpper(string word)
+        {
+            List<string> wordList = new List<string>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordList.Add(word);
+            }
+            string wordToUpper = word.ToUpper();
+            return wordToUpper;
+        }
+
+        static public string SentenceToUpper(string sentence)
+        {
+            string sentenceToUpper = sentence.ToUpper();
+            return sentenceToUpper;
+        }
+
+        static public string SentenceThroughWord(string sentence)
+        {
+            string throughWordToUpper = WordToUpper(sentence);
+            return throughWordToUpper;
+        }
+    }
+}
